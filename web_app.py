5f14932294967e7d46b208aa5e08007bea66fbc8
kuriangeorgebinu <kgeor040@uottawa.ca>
2021-12-22 12:59:20 -0500

    modified web_app.py with just username kgeor040@uottawa.ca

--- web_app.py
+++ web_app.py
@@ -3,20 +3,13 @@ from flask import Flask, request
 
 app = Flask(__name__) 
 
-# Disable caching 
-@app.after_request
-def set_response_headers(response): 
-	response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
-	response.headers['Pragma'] = 'no-cache'
-	response.headers['Expires'] = '0'
-	return response
 
 
 @app.route('/') 
 def hello():
-	user_email = requests.headers.get('X-Goog-Authenticated-User-Email') # IAP headers
-	user_id = requests.headers.get('X-Goog-Authenticated-User-ID') # IAP headers 
-	return render_template("hello.html", name="KGB", email=user_email, id=user_id) 
+	#user_email = requests.headers.get('X-Goog-Authenticated-User-Email') # IAP headers
+	#user_id = requests.headers.get('X-Goog-Authenticated-User-ID') # IAP headers 
+	return render_template("hello.html", name="KGB", email="kgeor040@uottawa.ca", id="kgb040") 
 
 
 
