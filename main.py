854a6bf7769fbbe31d2adaaac68d9267b12fc028
kuriangeorgebinu <kgeor040@uottawa.ca>
2021-12-22 15:12:05 -0500

    Added requirements.txt and modified main.py with IAP

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 
 @app.route('/') 
 def hello():
-	#user_email = requests.headers.get('X-Goog-Authenticated-User-Email') # IAP headers
-	#user_id = requests.headers.get('X-Goog-Authenticated-User-ID') # IAP headers 
+	user_email = request.headers.get('X-Goog-Authenticated-User-Email') # IAP headers
+	user_id = request.headers.get('X-Goog-Authenticated-User-ID') # IAP headers 
 	return render_template("hello.html", name="KGB", email="kgeor040@uottawa.ca", id="kgb040") 
 
 
